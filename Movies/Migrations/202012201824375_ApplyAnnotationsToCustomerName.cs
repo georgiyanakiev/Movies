9958b52f9cf06417a80cb662aca5c934aeddb3d9
Georgi Yanakiev <g.yanakiev@yahoo.com>
2020-12-20 20:21:43 +0000

    migrations and required properties

--- Movies/Migrations/202012201824375_ApplyAnnotationsToCustomerName.cs
+++ Movies/Migrations/202012201824375_ApplyAnnotationsToCustomerName.cs
@@ -3,11 +3,10 @@ namespace Movies.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class PopulateMembershipTypes : DbMigration
+    public partial class ApplyAnnotationsToCustomerName : DbMigration
     {
         public override void Up()
         {
-            
         }
         
         public override void Down()
