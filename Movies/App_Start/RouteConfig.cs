1abe3e853181b2f5a779470b397e5bd901063afe
Georgi Yanakiev <g.yanakiev@yahoo.com>
2020-12-20 11:34:05 +0000

    custom routes RouteConfig

--- Movies/App_Start/RouteConfig.cs
+++ Movies/App_Start/RouteConfig.cs
@@ -13,6 +13,13 @@ namespace Movies
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+
+            routes.MapRoute(
+                "MoviesByReleaseDate",
+                "movies/released/{year}/{month}",
+                new { controller = "Movies", action = "ByReleaseDate" },
+                new { year = @"2015|2016", month = @"\d{2}" });
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
--- Movies/Controllers/MoviesController.cs
+++ Movies/Controllers/MoviesController.cs
@@ -15,5 +15,27 @@ namespace Movies.Controllers
             var movie = new Movie() { Name = "Shrek!" } ;
             return View(movie);
         }
+
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+        public ActionResult Edit (int id)
+        {
+            return Content("id=" + id);
+        }
+
+        // movies 
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
     }
 }
