b3002a888c7d1e2f242e3ceeb17cc64c7ef20f86
Georgi Yanakiev <g.yanakiev@yahoo.com>
2020-12-20 11:43:28 +0000

    attribute routing

--- Movies/Controllers/MoviesController.cs
+++ Movies/Controllers/MoviesController.cs
@@ -15,7 +15,7 @@ namespace Movies.Controllers
             var movie = new Movie() { Name = "Shrek!" } ;
             return View(movie);
         }
-
+        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1, 12)}")]
         public ActionResult ByReleaseDate(int year, int month)
         {
             return Content(year + "/" + month);
