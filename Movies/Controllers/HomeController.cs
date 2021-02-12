c8dd45e17c11497885d282dc65f1a2c09989c846
Georgi Yanakiev <g.yanakiev@yahoo.com>
2021-02-12 15:26:36 +0200

    output cache

--- Movies/Controllers/HomeController.cs
+++ Movies/Controllers/HomeController.cs
@@ -4,13 +4,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI;
 
 namespace Movies.Controllers
 {
     public class HomeController : Controller
     {
         
-
+        [OutputCache(Duration = 50, Location = OutputCacheLocation.Server, VaryByParam = "*")]
         public ActionResult Netflix()
         {
            
